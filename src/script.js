663e486661c2ff97fb8fb80f1b48363975f1c30d
a.zagoveliev <63207977+mexell96@users.noreply.github.com>
2020-04-28 11:23:20 +0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,21 @@
+let bmw = {
+    price: "25000 USD", 
+    ageInSecond: 157664930684939685867399394n,
+    odometer: 110000,
+    awd: true, 
+};
+let mercedes = {
+    price: "35000 USD", 
+    ageInSecond: 100054493068493968586739394n,
+    odometer: 90000,
+    awd: true, 
+};
+let audi = {
+    price: "20000 USD", 
+    ageInSecond: 180000009306849396857399394n,
+    odometer: 140000,
+    awd: false, 
+};
+let garage = [bmw, mercedes, audi];
+
+console.log (garage);
